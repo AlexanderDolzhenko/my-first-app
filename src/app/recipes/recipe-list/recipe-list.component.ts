e29d318a979125778125844afad33757bc7b0fa7
Alexander <dolzhenkoa93@gmail.com>
2023-10-08 23:28:31 +0300

    111

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,5 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('a test recipe', 'test descr', 'https://i.pinimg.com/564x/d8/ff/75/d8ff752c79bb44db7b9b73c4d4e1103b.jpg'),
+    new Recipe('a test recipe', 'test descr', 'https://i.pinimg.com/564x/d8/ff/75/d8ff752c79bb44db7b9b73c4d4e1103b.jpg')
+  ];
 }
